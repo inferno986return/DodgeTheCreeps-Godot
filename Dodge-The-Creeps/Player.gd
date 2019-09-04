7bb90768f3c1ceb3c5a66209787a12fb9c8fd65f
Hal Motley <inferno986returns@gmail.com>
2019-09-04 22:30:03 +0100

    Started adding the mobs

--- Dodge-The-Creeps/Player.gd
+++ Dodge-The-Creeps/Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 # Variables
 export var speed = 400 #How fast the player moves in pixels/second.
 var screen_size #Size of window
@@ -7,10 +9,12 @@ var screen_size #Size of window
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	#hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2() #They player's movement vector
+	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_just_pressed("ui_left"):
@@ -24,3 +28,27 @@ func _process(delta):
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
+	
+	#Clamp the player so they cannot leave the playing area.
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+func _on_Player_body_entered(body):
+	hide() # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
